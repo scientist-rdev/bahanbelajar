0188fbfbadef5d1cde380dd3c474b1aff80bf19d
scientist-rdev <radohaz@gmail.com>
2026-03-20 15:48:16 +0700

    tambah paragraf di test/page.js

--- src/app/test/page.js
+++ src/app/test/page.js
@@ -3,6 +3,7 @@ const TestPage = () => {
     <div className="dcontent" id="dcontentT">
       <h1>Test Page</h1>
       <p>This is a simple test page.</p>
+      <p>This page is used to verify that the routing and page rendering are working correctly.</p>
     </div>
   );
 };
