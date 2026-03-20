b8edb6f5c910ab2e06da8395bf4bee50ddc580ab
scientist-rdev <radohaz@gmail.com>
2026-03-20 14:48:15 +0700

    tahap pertama branch test

---
+++ src/app/test/page.js
@@ -0,0 +1,10 @@
+const TestPage = () => {
+  return (
+    <div className="dcontent" id="dcontentT">
+      <h1>Test Page</h1>
+      <p>This is a simple test page.</p>
+    </div>
+  );
+};
+
+export default TestPage;
